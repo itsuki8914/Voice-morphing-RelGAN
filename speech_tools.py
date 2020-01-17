a6e866c50bc75afbfc50fea59e1b384edd8251aa
itk <maxashnitchts@yahoo.co.jp>
2020-01-17 11:05:08 +0900

    Update speech_tools.py

--- speech_tools.py
+++ speech_tools.py
@@ -279,36 +279,3 @@ def sample_train_data(dataset_A, nBatch, num_mcep=36, n_frames=128):
 
     return x, x2, x_atr, y, y_atr, z, z_atr
 
-"""
-def sample_train_data(dataset_A, dataset_B, n_frames=128):
-    num_samples = min(len(dataset_A), len(dataset_B))
-    train_data_A_idx = np.arange(len(dataset_A))
-    train_data_B_idx = np.arange(len(dataset_B))
-    np.random.shuffle(train_data_A_idx)
-    np.random.shuffle(train_data_B_idx)
-    train_data_A_idx_subset = train_data_A_idx[:num_samples]
-    train_data_B_idx_subset = train_data_B_idx[:num_samples]
-
-    train_data_A = list()
-    train_data_B = list()
-
-    for idx_A, idx_B in zip(train_data_A_idx_subset, train_data_B_idx_subset):
-        data_A = dataset_A[idx_A]
-        frames_A_total = data_A.shape[1]
-        assert frames_A_total >= n_frames
-        start_A = np.random.randint(frames_A_total - n_frames + 1)
-        end_A = start_A + n_frames
-        train_data_A.append(data_A[:, start_A:end_A])
-
-        data_B = dataset_B[idx_B]
-        frames_B_total = data_B.shape[1]
-        assert frames_B_total >= n_frames
-        start_B = np.random.randint(frames_B_total - n_frames + 1)
-        end_B = start_B + n_frames
-        train_data_B.append(data_B[:, start_B:end_B])
-
-    train_data_A = np.array(train_data_A)
-    train_data_B = np.array(train_data_B)
-
-    return train_data_A, train_data_B
-"""
