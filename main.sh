4a283d0f5c29df5b61eefd6fba728a1ed2a62509
itk <maxashnitchts@yahoo.co.jp>
2020-01-29 10:26:30 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 python preprocess1.py
 python preprocess2.py
 python train_relgan_vm.py
-# python eval_relgan_vm.py --source_label 1 --source_label 3 --interpolation 0.6
+# python eval_relgan_vm.py --source_label 1 --target_label 3 --interpolation 0.6
